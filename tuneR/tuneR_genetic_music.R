b27edea77e7617d94f225e68c3b4e3d969def68b
Izzy <yizhar.toren@shopify.com>
2019-06-17 17:07:02 +0200

    minor code style

--- tuneR/tuneR_genetic_music.R
+++ tuneR/tuneR_genetic_music.R
@@ -28,7 +28,7 @@ n <- str_length(prelude)
 frequencies <- tibble(
   n=-33:14,
   notes = c("C2", "C#2/Db2", "D2", "D#2/Eb2", "E2", "F2", "F#2/Gb2", "G2", "G#2/Ab2", "A2", "A#2/Bb2", "B2", "C3", "C#3/Db3", "D3", "D#3/Eb3", "E3", "F3", "F#3/Gb3", "G3", "G#3/Ab3", "A3", "A#3/Bb3", "B3", "C4", "C#4/Db4", "D4", "D#4/Eb4", "E4", "F4", "F#4/Gb4", "G4", "G#4/Ab4", "A4", "A#4/Bb4", "B4", "C5", "C#5/Db5", "D5", "D#5/Eb5", "E5", "F5", "F#5/Gb5", "G5", "G#5/Ab5", "A5", "A#5/Bb5", "B5"),
-  code=c(letters, toupper(letters))[1:48]
+  code = c(letters, toupper(letters))[1:48]
 ) %>% 
   mutate(frequency = round(440*(2^(1/12))^n,4))
 
